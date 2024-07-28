26dbb1383bc357d4d291f1d588bb2d0128b60ee3
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2024-07-28 02:34:50 +0100

    Update config.py

--- config.py
+++ config.py
@@ -14,5 +14,8 @@ class Config:
 
         self.warehouse = os.path.join(os.getcwd(), 'warehouse')
 
-        # A S3 parameters template
+        # A S3 (Simple Storage Service) parameters template
         self.s3_parameters_template = 'https://raw.githubusercontent.com/preliminaries/.github/master/profile/s3_parameters.yaml'
+
+        # A S3 prefix
+        self.s3_prefix = 'mileage/'
